f16f372e9ac04144bc5b38e8b7f67786850711d0
RAJ PATEL <rj268617@dal.ca>
2023-10-15 12:08:23 -0300

    feat: implemented GET suites api

---
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/service/ISuiteMasterService.java
@@ -0,0 +1,9 @@
+package com.group15.tourassist.service;
+
+import com.group15.tourassist.entity.SuiteMaster;
+
+import java.util.List;
+
+public interface ISuiteMasterService {
+    List<SuiteMaster> findAllSuites();
+}
