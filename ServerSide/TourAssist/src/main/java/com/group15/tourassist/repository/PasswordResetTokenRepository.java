9b957925c35fcd06769a0880ace61f532ce0fb33
gvazirani <gn745979@dal.ca>
2023-10-15 13:29:08 -0300

    feat: forgot password

---
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package com.group15.tourassist.repository;
+
+import com.group15.tourassist.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    PasswordResetToken findByToken(String token);
+
+}
---
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/service/PasswordResetTokenService.java
@@ -0,0 +1,56 @@
+package com.group15.tourassist.service;
+
+import com.group15.tourassist.core.config.service.TokenGenerator;
+import com.group15.tourassist.entity.AppUser;
+import com.group15.tourassist.entity.PasswordResetToken;
+import com.group15.tourassist.repository.IAppUserRepository;
+import com.group15.tourassist.repository.PasswordResetTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetTokenService<EmailService> implements IPasswordResetTokenService{
+
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final IAppUserRepository appUserRepository;
+
+    @Override
+    public String generatePasswordResetToken(AppUser appUser) {
+        String token= TokenGenerator.generateUniqueToken();
+        PasswordResetToken newpasswordResetToken= new PasswordResetToken();
+        newpasswordResetToken.setAppUser(appUser);
+        newpasswordResetToken.setToken(token);
+        LocalDateTime expiryDate= LocalDateTime.now().plusMinutes(60);
+        newpasswordResetToken.setExpiryDate(expiryDate);
+        passwordResetTokenRepository.save(newpasswordResetToken);
+        return token;
+    }
+
+    @Override
+    public Boolean validateToken(String token) {
+        PasswordResetToken passwordResetToken= passwordResetTokenRepository.findByToken(token);
+        return passwordResetToken!=null && !isPasswordResetTokenExpired(token) ;
+    }
+
+    @Override
+    public Boolean isPasswordResetTokenExpired(String token) {
+        PasswordResetToken passwordResetToken= passwordResetTokenRepository.findByToken(token);
+        var expiryDate=passwordResetToken.getExpiryDate();
+        LocalDateTime currentTime = LocalDateTime.now();
+        return expiryDate.isBefore(currentTime) ;
+    }
+
+    @Override
+    public void resetPassword(String token, String newPassword) {
+        PasswordResetToken passwordResetToken= passwordResetTokenRepository.findByToken(token);
+        AppUser user = passwordResetToken.getAppUser();
+        user.setPassword(passwordEncoder.encode(newPassword));
+        appUserRepository.save(user);
+        passwordResetTokenRepository.delete(passwordResetToken);
+    }
+}
