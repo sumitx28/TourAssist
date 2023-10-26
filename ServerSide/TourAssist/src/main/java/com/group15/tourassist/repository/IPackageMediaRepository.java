eb8d4a59b55c0292cfd729e4b277318b75e36dd4
RAJ PATEL <rj268617@dal.ca>
2023-10-26 09:59:32 -0300

    feat: unit test for PackageMedia save following TDD

---
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/repository/IPackageMediaRepository.java
@@ -0,0 +1,7 @@
+package com.group15.tourassist.repository;
+
+import com.group15.tourassist.entity.PackageMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPackageMediaRepository extends JpaRepository<PackageMedia, Long> {
+}
