f436397b16e8ade1f59d75098ceadbd986482200
Snehit Roda <sn584121@dal.ca>
2023-11-13 20:54:32 -0400

    chore: updated getActivityByPackageId to List of Activity ref #13

--- ServerSide/TourAssist/src/main/java/com/group15/tourassist/repository/IActivityRepository.java
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/repository/IActivityRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IActivityRepository extends JpaRepository<Activity, Long> {
 
     @Query("SELECT a FROM Activity a WHERE a.packageId = :packageId")
-    Activity getActivityDetailsByPackageId(@Param("packageId") Long packageId);
+    List<Activity> getActivityDetailsByPackageId(@Param("packageId") Long packageId);
 }
