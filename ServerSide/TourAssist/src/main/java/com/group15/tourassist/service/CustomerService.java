b953632c0450d8aa05871cba03ee720ca26c2825
RAJ PATEL <rj268617@dal.ca>
2023-11-16 21:18:20 -0400

    feat: implemented /customer api

--- ServerSide/TourAssist/src/main/java/com/group15/tourassist/service/CustomerService.java
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/service/CustomerService.java
@@ -1,13 +1,25 @@
 package com.group15.tourassist.service;
 
 import com.group15.tourassist.entity.Customer;
+import com.group15.tourassist.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService implements ICustomerService {
 
+    @Autowired
+    ICustomerRepository customerRepository;
+
+    /**
+     * @param appUserId appUserId to fetch customer details
+     * @return Customer details
+     */
     @Override
     public Customer getCustomerByAppUserId(Long appUserId) {
-        return null;
+        Optional<Customer> customerOptional = customerRepository.getCustomerByAppUserId(appUserId);
+        return customerOptional.orElse(null);
     }
 }
--- ServerSide/TourAssist/src/test/java/com/group15/tourassist/service/CustomerServiceTest.java
+++ ServerSide/TourAssist/src/test/java/com/group15/tourassist/service/CustomerServiceTest.java
@@ -1,33 +1,32 @@
 package com.group15.tourassist.service;
 
-import com.group15.tourassist.core.enums.BookedItem;
-import com.group15.tourassist.core.enums.BookingStatus;
 import com.group15.tourassist.core.enums.Role;
 import com.group15.tourassist.entity.AppUser;
-import com.group15.tourassist.entity.Booking;
 import com.group15.tourassist.entity.Customer;
-import com.group15.tourassist.entity.Guest;
-import com.group15.tourassist.request.BookingItemRequest;
-import com.group15.tourassist.request.BookingRequest;
+import com.group15.tourassist.repository.ICustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
     @InjectMocks
     private CustomerService customerService;
+
+    @Mock
+    private ICustomerRepository customerRepository;
 
     private AppUser appUser;
     private Customer customer;
@@ -40,8 +39,13 @@ class CustomerServiceTest {
 
     @Test
     void getCustomerByAppUserId() {
+        // Arrange
+        when(customerRepository.getCustomerByAppUserId(1L)).thenReturn(Optional.ofNullable(customer));
+
+        // Act
         Customer actualCustomer = customerService.getCustomerByAppUserId(1L);
 
+        //Assert
         assertEquals(customer, actualCustomer);
     }
 }
