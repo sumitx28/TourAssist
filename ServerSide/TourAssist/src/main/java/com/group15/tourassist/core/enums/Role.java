af97f35a6447a1ad4bc04a6c9093ab8a366218da
Snehit Roda <sn584121@dal.ca>
2023-11-17 20:47:28 -0400

    chore: code cleaned user type flow

--- ServerSide/TourAssist/src/main/java/com/group15/tourassist/core/enums/Role.java
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/core/enums/Role.java
@@ -1,15 +1,6 @@
 package com.group15.tourassist.core.enums;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static com.group15.tourassist.core.enums.Permission.*;
 
 /**
  * @author snehitroda
@@ -18,18 +9,7 @@ import static com.group15.tourassist.core.enums.Permission.*;
 @RequiredArgsConstructor
 public enum Role {
 
-    CUSTOMER(Collections.emptySet()),
-    AGENT(Collections.emptySet());
+    CUSTOMER(),
+    AGENT();
 
-    @Getter
-    private final Set<Permission> permissions;
-
-    public List<SimpleGrantedAuthority> getAuthorities() {
-        var authorities = getPermissions()
-                .stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toList());
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-        return authorities;
-    }
 }
