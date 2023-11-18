6bc91fd723d58c7f60fb97f47cca7083abfea7f8
Sumit Mansukhlal Savaliya <sm572004@dal.ca>
2023-11-17 20:47:48 -0400

    feat: payment and booking flow

--- ClientSide/src/components/travel-package/PaymentDialog.jsx
+++ ClientSide/src/components/travel-package/PaymentDialog.jsx
@@ -7,6 +7,10 @@ import {
   TextField,
   Button,
   Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
 import API_URL from "../../../config/config";
 import axios from "axios";
@@ -15,36 +19,80 @@ const PaymentDialog = ({ open, onClose, bookingData }) => {
   const [cardNumber, setCardNumber] = React.useState("");
   const [cvv, setCVV] = React.useState("");
   const [expiryDate, setExpiryDate] = React.useState("");
+  const [paymentType, setPaymentType] = React.useState("Credit Card");
+
+  const isValidCardNumber = (cardNumber) => {
+    return /^\d{16}$/.test(cardNumber);
+  };
+
+  const isValidCVV = (cvv) => {
+    return /^\d{3}$/.test(cvv);
+  };
+
+  const isValidExpiryDate = (expiryDate) => {
+    return /^\d{2}\/\d{4}$/.test(expiryDate);
+  };
 
   const handlePay = async () => {
-    console.log(bookingData);
+    if (
+      !isValidCardNumber(cardNumber) ||
+      !isValidCVV(cvv) ||
+      !isValidExpiryDate(expiryDate)
+    ) {
+      alert("Invalid card details. Please check your card information.");
+      return;
+    }
 
     alert("Processing payment...");
 
     const authToken = localStorage.getItem("authToken");
 
+    // Dummy successful and failed card details
+    const successfulCard = {
+      number: "4111111111111111",
+      cvv: "123",
+      expiryDate: "12/2024",
+    };
+
+    const failedCard = {
+      number: "4000123412341234",
+      cvv: "999",
+      expiryDate: "06/2022",
+    };
+
+    const isPaymentSuccessful =
+      cardNumber === successfulCard.number &&
+      cvv === successfulCard.cvv &&
+      expiryDate === successfulCard.expiryDate;
+
     const paymentData = {
       customerEmail: bookingData.user.sub,
       transactionId: Date.now(),
       bookingId: bookingData.bookingId,
-      paymentType: "Credit Card",
+      paymentType: paymentType,
       price: bookingData.totalPrice,
     };
 
     console.log(paymentData);
 
-    // const response = await axios.post(
-    //   `${API_URL}/api/v1/payment/payment-transaction`,
-    //   bookingData,
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${authToken}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // );
+    if (isPaymentSuccessful) {
+      paymentData.transactionStatus = "SUCCESS";
+      alert("Payment Successful");
+    } else {
+      paymentData.transactionStatus = "FAILED";
+      alert("Payment Failed. Please use valid card details.");
+    }
 
-    alert("Payment Successful");
+    const response = await axios.post(
+      `${API_URL}/api/v1/payment/payment-transaction`,
+      paymentData,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     onClose();
   };
@@ -53,6 +101,18 @@ const PaymentDialog = ({ open, onClose, bookingData }) => {
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Payment</DialogTitle>
       <DialogContent>
+        <FormControl fullWidth variant="outlined" margin="normal">
+          <InputLabel id="payment-type-label">Payment Type</InputLabel>
+          <Select
+            label="Payment Type"
+            value={paymentType}
+            onChange={(e) => setPaymentType(e.target.value)}
+            labelId="payment-type-label"
+          >
+            <MenuItem value="Credit Card">Credit Card</MenuItem>
+            <MenuItem value="Debit Card">Debit Card</MenuItem>
+          </Select>
+        </FormControl>
         <TextField
           label="Card Number"
           fullWidth
