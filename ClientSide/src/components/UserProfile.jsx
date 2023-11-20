2d06756197b98e666b64979caa0ecf0e816d774c
kumawat <dh243394@dal.ca>
2023-11-19 22:00:44 -0400

    feat: user profile api integration complete

--- ClientSide/src/components/UserProfile.jsx
+++ ClientSide/src/components/UserProfile.jsx
@@ -1,44 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import {jwtDecode} from 'jwt-decode'
+import NavBar from '../components/commons/NavBar';
 
 export default function UserProfile() {
-  // Example user data, replace the placeholder data with actual data
-  const userData = {
-    firstName: 'Jane',
-    lastName: 'Doe',
+  const [editFields, setEditFields] = useState({
     email: 'jane.doe@example.com',
-    mobileNumber: '123-456-7890',
-    dateOfBirth: '1990-01-01', // Format the date accordingly
-    country: 'Wonderland',
-    avatar: 'path_to_avatar_image', // Replace with path to user's avatar image
+    mobileNumber: '123-456-7890'
+  });
+
+  const [editMode, setEditMode] = useState(false);
+
+  const handleInputChange = (e) => {
+    setEditFields({ ...editFields, [e.target.name]: e.target.value });
+  };
+
+  const updateUserProfile = async (fieldToUpdate) => {
+    const authToken = localStorage.getItem("authToken");
+    const user = jwtDecode(authToken);
+    const apiEndpoint = 'http://localhost:8080/api/v1/user-profile/update-profile';
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${authToken}`
+    };
+
+    const data = {
+      appUserId: user.appUserId,
+      [fieldToUpdate]: editFields[fieldToUpdate]
+    };
+
+    try {
+      await axios.post(apiEndpoint, data, { headers });
+      alert('Profile updated successfully!');
+      setEditMode(false);
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      alert('Failed to update profile.');
+    }
+  };
+
+  const handleSaveEmail = () => {
+    updateUserProfile('email');
+  };
+
+  const handleSaveMobileNumber = () => {
+    updateUserProfile('mobile');
   };
 
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-wrap justify-center">
-        {/* Avatar & Name */}
-        <div className="w-full sm:w-1/2 md:w-1/4 p-2">
-          <div className="bg-white shadow rounded-lg p-4 text-center">
-            <img
-              className="w-32 h-32 rounded-full mx-auto"
-              src={userData.avatar}
-              alt={`${userData.firstName} ${userData.lastName}`}
-            />
-            <h2 className="text-xl font-semibold mt-2">{`${userData.firstName} ${userData.lastName}`}</h2>
-          </div>
-        </div>
-
-        {/* User Details */}
+        {/* ... existing avatar & name section ... */}
         <div className="w-full sm:w-1/2 md:w-3/4 p-2">
           <div className="bg-white shadow rounded-lg p-4">
             <h3 className="text-lg font-semibold mb-4">Personal Information</h3>
             <div className="space-y-3">
-              <p className="text-gray-600">{`Email: ${userData.email}`}</p>
-              <p className="text-gray-600">{`Mobile Number: ${userData.mobileNumber}`}</p>
-              <p className="text-gray-600">{`Date of Birth: ${userData.dateOfBirth}`}</p>
-              <p className="text-gray-600">{`Country: ${userData.country}`}</p>
-              <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-                Edit Profile
-              </button>
+              {editMode ? (
+                <>
+                  <input
+                    type="text"
+                    name="email"
+                    value={editFields.email}
+                    onChange={handleInputChange}
+                    className="border border-gray-300 rounded-md py-2 px-4 w-full"
+                  />
+                  <input
+                    type="text"
+                    name="mobileNumber"
+                    value={editFields.mobileNumber}
+                    onChange={handleInputChange}
+                    className="border border-gray-300 rounded-md py-2 px-4 w-full"
+                  />
+                  <button onClick={handleSaveEmail} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    Save Email
+                  </button>
+                  <button onClick={handleSaveMobileNumber} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    Save Mobile Number
+                  </button>
+                </>
+              ) : (
+                <>
+                  <p className="text-gray-600">{`Email: ${editFields.email}`}</p>
+                  <p className="text-gray-600">{`Mobile Number: ${editFields.mobileNumber}`}</p>
+                  <button onClick={() => setEditMode(true)} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    Edit Profile
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
