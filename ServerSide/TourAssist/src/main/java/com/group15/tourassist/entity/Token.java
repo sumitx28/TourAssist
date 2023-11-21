056ba588bcb894b2ccab5051ada1be9611df6f9d
Snehit Roda <sn584121@dal.ca>
2023-11-21 18:42:34 -0400

    chore: refactor design smell def encapsulation

--- ServerSide/TourAssist/src/main/java/com/group15/tourassist/entity/Token.java
+++ ServerSide/TourAssist/src/main/java/com/group15/tourassist/entity/Token.java
@@ -7,12 +7,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 /**
+ * Represents a token entity used to store JWT stateless authentication tokens associated with a user.
+ *
+ * <p>
+ * This class is annotated with Lombok annotations to generate common boilerplate code.
+ * - {@code @Data}: Generates getters, setters, toString, equals, and hashCode methods.
+ * - {@code @Builder}: Provides a builder pattern for easy object creation.
+ * - {@code @NoArgsConstructor}: Generates a default constructor with no parameters.
+ * - {@code @AllArgsConstructor}: Generates a constructor with all class fields as parameters.
+ * </p>
+ *
  * @author snehitroda
  * @implNote Token entity is used to store the JWT stateless auth tokens associated with a user
- * */
-
+ */
 @Data
 @Builder
 @NoArgsConstructor
@@ -20,21 +28,22 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Token {
 
-  @Id
-  @GeneratedValue
-  public Integer tokenId;
+    @Id
+    @GeneratedValue
+    private Integer tokenId;
 
-  @Column(unique = true)
-  public String token;
+    @Column(unique = true)
+    private String token;
 
-  @Enumerated(EnumType.STRING)
-  public TokenType tokenType = TokenType.BEARER;
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType = TokenType.BEARER;
 
-  public boolean isRevoked;
+    private boolean isRevoked;
 
-  public boolean isExpired;
+    private boolean isExpired;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "app_user_id")
-  public AppUser appUser;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "app_user_id")
+
+    private AppUser appUser;
 }
