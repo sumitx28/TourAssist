11000d1f96af810688a2c4dd63fd5db073ea2b44
Sumit Mansukhlal Savaliya <savaliyasumit717@gmail.com>
2024-04-24 20:15:59 -0300

    fix: sql init with activities controller improved response

--- TourAssist-backend/src/main/java/com/group15/tourassist/web/controller/ActivityMasterController.java
+++ TourAssist-backend/src/main/java/com/group15/tourassist/web/controller/ActivityMasterController.java
@@ -1,10 +1,13 @@
 package com.group15.tourassist.web.controller;
 
+import com.group15.tourassist.core.utils.ConstantUtils;
 import com.group15.tourassist.entity.ActivityMaster;
+import com.group15.tourassist.response.ActivitiesResponse;
 import com.group15.tourassist.service.impl.IActivityMasterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +24,13 @@ public class ActivityMasterController {
     private IActivityMasterService activityMasterService;
 
     @GetMapping("/activities")
-    private ResponseEntity<List<ActivityMaster>> getAllActivities() {
-        log.info("** get activities request");
+    private ResponseEntity<ActivitiesResponse> getAllActivities() {
+        log.info("Received request to get all activities");
         List<ActivityMaster> activities = activityMasterService.findAllActivities();
-        return ResponseEntity.of(Optional.of(activities));
+        ActivitiesResponse response = new ActivitiesResponse(activities);
+        response.setMessage(ConstantUtils.SUCCESS);
+        response.setStatusCode("200");
+        log.info("The request to retrieve activities was handled.");
+        return ResponseEntity.ok(response);
     }
 }
